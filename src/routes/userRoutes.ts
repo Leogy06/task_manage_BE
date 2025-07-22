d28b2b30560a736257f468c4afe4fea1ccb23391
Leogy06 <2022_cete_santillanlb@online.htcgsc.edu.ph>
2025-07-22 09:45:22 +0800

    mamking it into default

--- src/routes/userRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { createUserController } from "../controllers/userController.js";
-
-const userRoutes = Router();
-
-userRoutes.post("/create", createUserController);
-
-export default userRoutes;
--- src/utils/token.ts
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-import "../config/environment.js";
-import { UserTypes } from "../types/user.js";
-
-export const generateToken = (userId: UserTypes["id"]) => {
-  return jwt.sign({ userId }, process.env.JWT_SECRET as string, {
-    expiresIn: "1h",
-  });
-};
