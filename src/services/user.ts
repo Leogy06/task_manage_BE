ed9a020c0e26c2cc6a0d9607b8e6cdf5ef1a16f2
Leogy06 <2022_cete_santillanlb@online.htcgsc.edu.ph>
2025-09-25 12:14:38 +0800

    implement user creation service with validation and password hashing

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,9 +1,23 @@
 import prisma from "../config/prismaConfig.js";
-import { CreateUserInput } from "../validations/userSchema.js";
+import { hashPassword } from "../utils/hasPassword.js";
+import {
+  CreateUserInput,
+  createUserSchema,
+} from "../validations/userSchema.js";
 
 export const createUserService = async (data: CreateUserInput) => {
+  const validatedData = createUserSchema.safeParse(data);
+  if (!validatedData.success) {
+    throw validatedData.error;
+  }
+
+  const { password, ...rest } = validatedData.data;
+
   const newUser = await prisma.users.create({
-    data,
+    data: {
+      ...rest,
+      ...(password && { password: await hashPassword(password) }),
+    },
   });
 
   return newUser;
