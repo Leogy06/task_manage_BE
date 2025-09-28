9b875ce93e225e64d0187b11665c6557028e463f
Leogy06 <2022_cete_santillanlb@online.htcgsc.edu.ph>
2025-09-28 14:51:43 +0800

    refactor password validation message in user schema

--- src/validations/userSchema.ts
+++ src/validations/userSchema.ts
@@ -6,7 +6,10 @@ export const createUserSchema = z.object({
   middle_name: z.string().max(20).optional(),
   suffix: z.string().max(5).optional(),
   username: z.string().min(5).max(36),
-  password: z.string().min(8).max(20),
+  password: z
+    .string()
+    .min(8, "Password should be atleast 8 characters.")
+    .max(20),
 });
 
 export type CreateUserInput = z.infer<typeof createUserSchema>;
